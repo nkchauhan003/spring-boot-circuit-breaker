d9a96942b5398887c82cc4d07f67c1f1956f8bcf
nkchauhan003 <nkchauhan003@gmail.com>
2023-01-14 12:47:50 +0530

    added new code

--- order-service/src/main/java/com/codeburps/client/product/ProductClientImpl.java
+++ order-service/src/main/java/com/codeburps/client/product/ProductClientImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.HashMap;
 import java.util.Map;
 
